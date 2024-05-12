e4a1dfae3e9e0a0a8dfa3c87087bb68535c90295
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-12 13:24:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as new version
+
+// alert( 3 + 3 ) //we are using node.js, not browser
+//also manage code readability
+
+let name = "anshuman" //string
+let age = 18 //number
+let isLoggedIn = true //boolean
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

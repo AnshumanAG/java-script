54d59ecc2ba5b913886a6f2d27cecf1fbefad122
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-13 19:16:46 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,50 @@
+ // primitive
+
+ // 7 types : string, Number, Boolean, null, undefined, BigInt, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIN = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const bigNumber = 378234682472n
+
+
+ //Referencce (non-Primitive)
+
+ //Array, Objects, Functions
+
+ const heros = ["shaktiman", "naagraj", "doga"];
+
+ let myobj = {
+    name : "anshuman",
+    age : 20,
+ }
+
+const myFunction = function(){
+    console.log("hello world");
+} 
+
+console.log(typeof bigNumber);
+
+/* typeof ==
+*****primitive*******
+undefined = undefined
+null = object
+boolean = boolean
+number = number
+string = string
+object = object
+bigint = bigint
+
+*****non-primitive******
+arrays = object
+function = function
+object = object

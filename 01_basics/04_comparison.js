63cb6ca15bcf66a03133fb617c6d91247c8f8f0f
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-14 10:36:04 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,14 +8,36 @@
 // console.log("02" > 1);
 
 // ***********this type of comparsion we need to avoid **********************
-console.log(null > 0);
-console.log(null == 0); //different working
-console.log(null >= 0); // different working
+// console.log(null > 0);
+// console.log(null == 0); //different working
+// console.log(null >= 0); // different working
 
-console.log(undefined > 0);
-console.log(undefined == 0);
-console.log(undefined >= 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
 
 // === (strictly check)
 
-console.log("2" === 2);
+// console.log("2" === 2);
+
+
+//****************************************************************************************
+
+// stack (primitive), Heap (non-primitive)
+// stack me copy milta hai
+// heap me reference milta hai
+let myYoutubeName = "anshumanNobbiGaming"
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+
+let userOne = {
+    email : "anshu@gmail.com",
+    Upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "nobbi@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);

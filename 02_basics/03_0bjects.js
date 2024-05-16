02280ce7921e7b99dcb21aefeeb667b7415af15e
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-16 12:27:22 +0000

    objects

---
+++ 02_basics/03_0bjects.js
@@ -0,0 +1,16 @@
+// singleton
+// object.create
+
+// object literals
+const jsUser = {
+    name: "anshuman",
+    // "full name": "anshuman agarwal",
+    age: 18,
+    email: "anshuma@gmail.com",
+    location: "alwar",
+    isLoggedIn: false,
+    lastLoginDays: ["saturday", "monday"]
+}
+console.log(jsUser.email);
+// console.log(jsUser["email"]); // same same but at a string time this is correct
+// console.log(jsUser["full name"]);

2f9a0e7d1b2ba6fc25360cdc0e9ddac9a015b361
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-20 08:57:47 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,6 @@
+// curly bressis is scope
+
+
 // var c = 300
 let a = 300 // global scope
 
@@ -5,11 +8,52 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("inner :", a); //block scope
+    // console.log("inner :", a); //block scope
 }
 
 
 
- console.log(a);
+//  console.log(a);
 //  console.log(b);
 //  console.log(c);
+
+
+function one() {
+    const username = "anshuman"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+
+
+if (true) {
+    const username = "anshuman"
+    if (true) {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+//************************** intresting **********************
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+const addtwo = function (num){
+    return num + 2
+}
+console.log(addtwo(5))

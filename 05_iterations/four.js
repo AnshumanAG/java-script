dcc44aecde5ce0b52eaba539f286bdbd00e6cba2
ANSHUMAN AGRAWAL <139856692+AnshumanAG@users.noreply.github.com>
2024-05-24 09:45:15 +0000

    forin loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,22 @@
+// forin loop *****************
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('USA', "united states of america")
+// map.set('FR', "france")
+
+// for (const key in map) {
+    
+//     console.log(key);
+// }
